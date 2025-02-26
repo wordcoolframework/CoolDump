74c2ca92c85eb51a52a548cc655ffffd140d06a6
wordcoolframework <wordcool.framework@gmail.com>
2025-02-26 07:21:43 -0800

    first commit

--- src/helper.php
+++ src/helper.php
@@ -3,15 +3,19 @@
 use CoolDump\CoolDump;
 
 if (!function_exists('wc')){
+
     function wc(...$data) : void {
         $wtf = new CoolDump();
         $wtf->wc(...$data);
     }
+
 }
 
 if(!function_exists('wcJson')){
+
     function wcJson(...$data) : void{
         $wtf = new CoolDump();
         $wtf->wcJson(...$data);
     }
+
 }
